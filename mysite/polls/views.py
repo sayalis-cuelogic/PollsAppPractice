602b5c8626257cc33a2fed759b46e4e3c5632564
sayalis-cuelogic <sayali.shete@cuelogic.co.in>
2015-08-27 14:38:16 +0530

    created tests for pub_date of question

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -4,6 +4,7 @@ from django.template import RequestContext, loader
 from django.shortcuts import get_object_or_404, render
 from django.core.urlresolvers import reverse
 from django.views import generic
+from django.utils import timezone
 
 from polls.models import Question
 # Create your views here.
@@ -30,8 +31,10 @@ class IndexView(generic.ListView):
     context_object_name = 'latest_question_list'
 
     def get_queryset(self):
-        """Return the last five published questions."""
-        return Question.objects.order_by('-pub_date')[:5]
+
+        return Question.objects.filter(pub_date__lte=timezone.now()).order_by(
+                                      '-pub_date')[:5]
+
 
 # def detail(request, question_id):
 #     # try:
@@ -47,6 +50,10 @@ class IndexView(generic.ListView):
 class DetailView(generic.DetailView):
     model = Question
     template_name = 'polls/detail.html'
+
+    def get_queryset(self):
+        
+        return Question.objects.filter(pub_date__lte=timezone.now())
 
 
 class ResultsView(generic.DetailView):
